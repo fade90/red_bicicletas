f97e83d08ef8cb7059a4a204b063335792f48a1d
Federico Rodriguez <fede90.12@gmail.com>
2020-09-29 20:51:54 -0300

    Detalles

--- config/passport.js
+++ config/passport.js
@@ -20,7 +20,7 @@ passport.use(new localStrategy(
 passport.use(new GoogleStrategy({
     clientID: process.env.GOOGLE_CLIENT_ID,
     clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-    callbackURL: "https://red-bike.herokuapp.com/auth/google/callback"
+    callbackURL: process.env.HOST + "/auth/google/callback"
 },
     function(accessToken, refreshToken, profile, cb) {
         console.log(profile);
