42613aa729e8076428e8714dd8791c7a8161230f
Federico Rodriguez <fede90.12@gmail.com>
2020-09-29 22:29:20 -0300

    Detalles

--- app.js
+++ app.js
@@ -159,10 +159,8 @@ app.use('/googlebec72500f75c9e45', function (req, res) {
 });
 
 app.get('/auth/google',
-  passport.authenticate('google', { scope: 
-      [ 'https://www.googleapis.com/auth/plus.login',
-      , 'https://www.googleapis.com/auth/plus.profile.emails.read' ] }
-));
+  passport.authenticate('google', { scope: ['profile', 'email'] })
+);  
 
 app.get( '/auth/google/callback', passport.authenticate( 'google', { 
         successRedirect: '/',
